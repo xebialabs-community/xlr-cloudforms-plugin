958d8ed6e871728ddf2984cd62142862ee8878e3
Benoit Moussaud <bmoussaud@xebialabs.com>
2018-11-12 11:00:52 +0100

    add the check service task

--- src/main/resources/CloudForms/cloudform_check_connection.py
+++ src/main/resources/CloudForms/cloudform_check_connection.py
@@ -11,40 +11,17 @@
 import requests
 from requests.auth import HTTPBasicAuth
 
-if cloudformsServer is None:
-    print
-    "No cloudform server provided."
-    sys.exit(1)
-
 headers = {"Accept": "application/json", "Content-Type": "application/json"}
-service_template_url = "{0}/api/service_templates/{1}".format(cloudformsServer['url'], serviceTemplate)
-print('* url: {0}'.format(service_template_url))
+url = "{0}/api".format(configuration.url)
+print('* url: {0}'.format(url))
 
-body_data = {
-    'action': 'order',
-    'resource': {
-        'href': service_template_url
-    }
-}
-# print (body_data)
+r = requests.get(url,
+                 headers=headers,
+                 verify=False,
+                 auth=(cloudformsServer.username, cloudformsServer.password))
 
-r = requests.post(service_template_url,
-                  json=body_data,
-                  headers=headers,
-                  verify=False,
-                  auth=(cloudformsServer['username'], cloudformsServer['password']))
 if r.status_code == requests.codes.ok:
     response = r.json()
-    serviceRequestsId = response['id']
-    serviceRequestState = response['request_state']
-    serviceRequestMessage = response['message']
-
-    print("* serviceRequestId {0}".format(serviceRequestsId))
-    print("* serviceRequestState {0}".format(serviceRequestState))
-    print("* serviceRequestMessage {0}".format(serviceRequestMessage))
-    print("* href {0}".format(response['href']))
-
-    task.setStatusLine("{0}:{1}".format(serviceRequestState, serviceRequestMessage))
-    task.schedule("cloudforms/wait-for-end-service-request.py")
+    print("* CFME version: " + response['server_info']['version'])
 else:
-    raise Exception("%s: HTTP response code %s (%s)" % (service_template_url, r.status_code, r.json()))
+    raise Exception("%s: HTTP response code %s (%s)" % (url, r.status_code, r.json()))
